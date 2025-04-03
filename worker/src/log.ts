c03e599c1e7d46d04da5ed9a5474f234c876bec0
John Hurliman <jhurliman@jhurliman.org>
2025-04-03 00:07:09 -0700

    feat: use "status" string instead of "level" in production pino loggers

--- worker/src/log.ts
+++ worker/src/log.ts
@@ -5,7 +5,14 @@ import { IS_PRODUCTION, IS_TEST } from "./environment"
 const level = IS_TEST ? "warn" : IS_PRODUCTION ? "info" : "debug"
 
 export const log = IS_PRODUCTION
-  ? pino({ level })
+  ? pino({
+      level,
+      formatters: {
+        level(label) {
+          return { status: label }
+        },
+      },
+    })
   : pino({
       level,
       transport: {
