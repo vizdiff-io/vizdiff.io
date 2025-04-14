71b5374c8d414f30c228ba69ec823f697bec3ae2
John Hurliman <jhurliman@jhurliman.org>
2025-04-14 14:20:24 -0700

    test: add /pricing stories

---
+++ frontend/src/stories/pricing.stories.tsx
@@ -0,0 +1,69 @@
+import type { Meta, StoryObj, StoryContext } from "@storybook/react"
+import { type ComponentType } from "react"
+
+import ThemeWrapper from "./ThemeWrapper"
+import { catchAllHandler, userHandler } from "./mocks"
+import PricingComponent from "../pages/pricing"
+
+type StoryArgs = {
+  mode?: "light" | "dark"
+  isAuthenticated?: boolean
+}
+
+const meta: Meta<typeof PricingComponent> = {
+  title: "stories/pages/Pricing",
+  component: PricingComponent,
+  argTypes: {
+    mode: {
+      control: "radio",
+      options: ["light", "dark"],
+      defaultValue: "light",
+    },
+    isAuthenticated: {
+      control: "boolean",
+      defaultValue: false,
+    },
+  },
+  decorators: [
+    (Story: ComponentType, context: StoryContext<StoryArgs>): JSX.Element => (
+      <ThemeWrapper
+        mode={context.args.mode ?? "light"}
+        isAuthenticated={context.args.isAuthenticated}
+      >
+        <Story />
+      </ThemeWrapper>
+    ),
+  ],
+  parameters: {
+    msw: {
+      handlers: [userHandler, catchAllHandler],
+    },
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof PricingComponent>
+
+export const Light: Story = {
+  args: {
+    mode: "light",
+    isAuthenticated: false,
+  },
+  parameters: { msw: { handlers: [catchAllHandler] } },
+}
+
+export const Dark: Story = {
+  args: {
+    mode: "dark",
+    isAuthenticated: false,
+  },
+  parameters: { msw: { handlers: [catchAllHandler] } },
+}
+
+export const Authenticated: Story = {
+  args: {
+    mode: "light",
+    isAuthenticated: true,
+  },
+  parameters: { msw: { handlers: [catchAllHandler] } },
+}
